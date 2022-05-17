e663aea31de0da50c5fe6df83c6109081a389272
mhrzica <mateja.hrzica@gmail.com>
2022-05-17 18:33:46 +0200

    Increased batch size to 20

--- run_eval.sh
+++ run_eval.sh
@@ -8,4 +8,4 @@ nvidia-smi
 echo "#######################################################"
 echo "Run demo evaluation"
 echo "#######################################################"
-python3 eval_mateja.py --dataset sunrgbd --use_shapenet --checkpoint_path /votenet/output/Dataset_220322_all_classes_v3_changed_predictions_IoU/checkpoint.tar --dump_dir /tmp --use_cls_nms --use_3d_nms --num_point 50000 --batch_size 32 --nms_iou 0.1 --conf_thresh 0.75 --cluster_sampling seed_fps
+python3 eval_mateja.py --dataset sunrgbd --use_shapenet --checkpoint_path /votenet/output/Dataset_220322_all_classes_v3_changed_predictions_IoU/checkpoint.tar --dump_dir /tmp --use_cls_nms --use_3d_nms --num_point 50000 --batch_size 20 --nms_iou 0.1 --conf_thresh 0.75 --cluster_sampling seed_fps
